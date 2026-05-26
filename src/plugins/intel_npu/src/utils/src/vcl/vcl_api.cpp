676db42d4d2a24b419632e4eea54f33fb01aaa21
Zhu, Shaojie <shaojie.zhu@intel.com>
2026-05-26 14:51:00 +0800

    test different loader path
    Signed-off-by: Zhu, Shaojie <shaojie.zhu@intel.com>

--- src/plugins/intel_npu/src/utils/src/vcl/vcl_api.cpp
+++ src/plugins/intel_npu/src/utils/src/vcl/vcl_api.cpp
@@ -4,18 +4,47 @@
 
 #include "intel_npu/utils/vcl/vcl_api.hpp"
 
+#include <cstdlib>
+#include <map>
 #include <mutex>
 
 #include "openvino/util/file_util.hpp"
 #include "openvino/util/shared_object.hpp"
 
 namespace intel_npu {
+
+namespace {
+constexpr auto ALT_DEPENDENCY_PATH_ENV_NAME = "NPU_ALT_DEPENDENCY_PATH";
+constexpr auto COMPILER_TYPE_ENV_NAME = "NPU_COMPILER_TYPE";
+constexpr auto DRIVER_COMPILER_TYPE_NAME = "DRIVER";
+
+std::string_view getActiveCompilerType() {
+    if (const auto* compilerType = std::getenv(COMPILER_TYPE_ENV_NAME);
+        compilerType != nullptr && *compilerType != '\0') {
+        return compilerType;
+    }
+    return {};
+}
+
+std::filesystem::path getCompilerLibraryPath() {
+    if (getActiveCompilerType() == DRIVER_COMPILER_TYPE_NAME) {
+        if (const auto* customPath = std::getenv(ALT_DEPENDENCY_PATH_ENV_NAME);
+            customPath != nullptr && *customPath != '\0') {
+            return std::filesystem::path(customPath);
+        }
+    }
+
+    return ov::util::get_ov_lib_path();
+}
+}  // namespace
+
 VCLApi::VCLApi() : _logger("VCLApi", Logger::global().level()) {
+    const auto compilerLibraryPath = getCompilerLibraryPath();
     const auto baseName = "openvino_intel_npu_compiler_loader";
 
     try {
-        const auto libpath = ov::util::make_plugin_library_name(ov::util::get_ov_lib_path(), baseName);
-        _logger.debug("Try to load %s", baseName);
+        const auto libpath = ov::util::make_plugin_library_name(compilerLibraryPath, baseName);
+        _logger.debug("Try to load: %s", ov::util::path_to_string(libpath).c_str());
         this->lib = ov::util::load_shared_object(libpath);
     } catch (const std::runtime_error& error) {
         _logger.debug("Failed to load %s: %s", baseName, error.what());
